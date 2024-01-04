ffc1a25ecad6e69fd355635228bf37fa807634e8
Ihor <ikruzhynskyy@gmail.com>
2024-01-04 22:27:36 +0200

    worked with linear gradient and added image to the background in startGameScreen

--- App.tsx
+++ App.tsx
@@ -1,16 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground, Text, View } from 'react-native';
 import { StartGameScreen } from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="auto" />
-      <StartGameScreen />
-    </>
+    <LinearGradient colors={['#4e0329', '#ddb53f']}  style={styles.rootScreen}>
+      <ImageBackground 
+        source={require('./assets/images/purple-pictures-zvo912urb2ectn70.png')} 
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgrouImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
- 
+  rootScreen: {
+    flex: 1,
+    // backgroundColor: '#ddb53f'
+  },
+  backgrouImage: {
+    opacity :0.15
+  }
 });
